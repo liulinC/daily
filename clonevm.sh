e16e7cfa8e4d4bbf70d5da18b4cdc08d325bdeaa
root <root@xrtuk-12-02.xenrtcloud>
2020-03-04 08:37:51 +0000

    add start, stop script
    Signed-off-by: root <root@xrtuk-12-02.xenrtcloud>

--- clonevm.sh
+++ clonevm.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 NEWNAME=""
-for ((c=0; c<=400; c++))
+NUM=10
+if [ $# -eq 1 ]; then
+	NUM=$1
+fi
+for ((c=0; c<=$NUM; c++))
 do
 	NEWNAME="dlvm"$c
 	xe vm-clone vm=Citrix\ Demonstration\ Linux\ Virtual\ Machine  new-name-label=$NEWNAME
---
+++ stop.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+NEWNAME=""
+NUM=10
+if [ $# -eq 1 ]; then
+        NUM=$1
+fi
+for ((c=0; c<=$NUM; c++))
+do
+	NEWNAME="dlvm"$c
+	xe vm-shutdown vm=$NEWNAME &
+done
+
