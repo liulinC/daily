4f89848932f13907e95421f7802f1ae1d56f6f5a
Lin Liu <lin.liu@citrix.com>
2020-04-10 07:56:39 +0000

    add xaip debug
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

---
+++ xapi/debug.sh
@@ -0,0 +1,6 @@
+sshpass -p $2 scp ./_build/default/ocaml/xapi/xapi_main.exe root@$1.xenrt.citrite.net:/root
+sshpass -p $2 ssh root@$1.xenrt.citrite.net "service xapi stop"
+sshpass -p $2 ssh root@$1.xenrt.citrite.net "cp -f /root/xapi_main.exe /opt/xensource/bin/xapi"
+sshpass -p $2 ssh root@$1.xenrt.citrite.net "md5sum /opt/xensource/bin/xapi"
+md5sum ./_build/default/ocaml/xapi/xapi_main.exe
+sshpass -p $2 ssh root@$1.xenrt.citrite.net "service xapi start"
