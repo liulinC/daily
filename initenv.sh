ebc289eafd98ab06a6d502be7ecc3a1ae309f4a0
Lin Liu <lin.liu@citrix.com>
2021-07-22 02:52:45 -0400

    run as non root
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

--- initenv.sh
+++ initenv.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "init tmux"
-yum install -y tmux
+sudo yum install -y tmux
 
 # install vim
 echo "init vim"
-yum install -y vim || true
+sudo yum install -y vim || true
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim ||true
 cp -f ./.vimrc ~/.vimrc
 
